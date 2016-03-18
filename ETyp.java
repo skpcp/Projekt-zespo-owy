a80464c806df870ee4803c6664260b9b81af3a8e
SK.PCP <sk.pcp@wp.pl>
2016-03-18 14:07:33 +0100

    Enumy

---
+++ ETyp.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * Created by bidzis on 2016-03-18.
+ */
+public enum ETyp {
+    WEWNETRZNY,
+    ZEWNETRZNY,
+}
---
+++ ETypZgloszenia.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created by bidzis on 2016-03-18.
+ */
+public enum ETypZgloszenia {
+    TRYWIALNY,
+    WAZNY,
+    KRYTYCZNY,
+    BLOKUJACY
+}

54f2e5dfb3c010cb7a78a14a687f13d354210d1f
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2016-05-27 15:40:48 +0200

    Poprawki binaria

--- src/main/java/com/uwm/projektz/attachment/api/AttachmentController.java
+++ src/main/java/com/uwm/projektz/attachment/api/AttachmentController.java
@@ -8,10 +8,8 @@ import com.uwm.projektz.enums.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.util.List;
 
 /**
@@ -23,13 +21,13 @@ import java.util.List;
 public class AttachmentController {
 
     @Autowired
-    IAttachmentSerivce attachmentSerivce;
+    IAttachmentSerivce attachmentService;
 
     @RequestMapping(value ="/saveAttachment", method = RequestMethod.POST,consumes ="application/json", produces = "application/json")
     @ResponseBody
     public ResponseEntity<AttachmentDTO> saveAttachment(@RequestBody AttachmentDTOCreate aAttachmentDTO){
         try {
-            return new ResponseEntity<>(attachmentSerivce.saveAttachment(aAttachmentDTO), HttpStatus.OK);
+            return new ResponseEntity<>(attachmentService.saveAttachment(aAttachmentDTO), HttpStatus.OK);
         }catch (MyServerException e){
             return new ResponseEntity<>(e.getHeaders(),e.getStatus());
         }
@@ -38,51 +36,51 @@ public class AttachmentController {
     @RequestMapping(value = "/getAttachmentById/{id}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<AttachmentDTO> findAttachmentById(@PathVariable("id") Long aId){
-        return new ResponseEntity<>(attachmentSerivce.findAttachmentById(aId), HttpStatus.OK);
+        return new ResponseEntity<>(attachmentService.findAttachmentById(aId), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/getAllAtachments", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<AttachmentDTO>> findAttachments(){
-        return new ResponseEntity<>(attachmentSerivce.findAllAttachment(), HttpStatus.OK);
+        return new ResponseEntity<>(attachmentService.findAllAttachment(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/getByName/{name}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<AttachmentDTO>> findAttachmentsByName(@PathVariable("name")String aName){
-        return new ResponseEntity<>(attachmentSerivce.findAttachmentByName(aName), HttpStatus.OK);
+        return new ResponseEntity<>(attachmentService.findAttachmentByName(aName), HttpStatus.OK);
     }
 
 
     @RequestMapping(value = "/getByMineType/{mine_type}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<AttachmentDTO>> findAttachmentsByMineType(@PathVariable("mine_type")String aMineType){
-        return new ResponseEntity<>(attachmentSerivce.findAttachmentByMineType(aMineType), HttpStatus.OK);
+        return new ResponseEntity<>(attachmentService.findAttachmentByMineType(aMineType), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/getByFileName/{file_name}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<AttachmentDTO>> findAttachmentsByFileName(@PathVariable("file_name")String aFileName){
-        return new ResponseEntity<>(attachmentSerivce.findAAtachmentsByFileName(aFileName), HttpStatus.OK);
+        return new ResponseEntity<>(attachmentService.findAAtachmentsByFileName(aFileName), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/getByMineTypeAndName/{mine_type},{name}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<AttachmentDTO>> findAAtachemntByMineTypeAndName(@PathVariable("mine_type")String aMineType,@PathVariable("name") String aName){
-        return new ResponseEntity<>(attachmentSerivce.findAAtachemntByMineTypeAndName(aMineType,aName), HttpStatus.OK);
+        return new ResponseEntity<>(attachmentService.findAAtachemntByMineTypeAndName(aMineType,aName), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/getByType/{type}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<AttachmentDTO>> findAttachmentByType(@PathVariable("type") Type aType){
-        return new ResponseEntity<>(attachmentSerivce.findAttachmentByType(aType), HttpStatus.OK);
+        return new ResponseEntity<>(attachmentService.findAttachmentByType(aType), HttpStatus.OK);
     }
 
-    @RequestMapping(value= "/removAttachmentById/{id}",method = RequestMethod.POST)
+    @RequestMapping(value= "/removeAttachmentById/{id}",method = RequestMethod.POST)
     @ResponseBody
     public ResponseEntity<Void> deleteAttachment(@PathVariable("id")Long aId)
     {
-        attachmentSerivce.deleteAttachmentById(aId);
+        attachmentService.deleteAttachmentById(aId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
